e3c04837d672a86f472ecb59f06e848904fecd81
rsagar1903 <rsagar1903@gmail.com>
2025-11-14 20:25:27 +0530

    Bank frontend fineshed

--- BANKv2BANK/lib/main.dart
+++ BANKv2BANK/lib/main.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/login_page.dart';
+import 'screens/registration.dart';
+import 'observer.dart';
 
 void main() {
-  runApp(MyBasicApp());
+  runApp(MyBankApp());
 }
 
-class MyBasicApp extends StatelessWidget {
+class MyBankApp extends StatefulWidget {
+  @override
+  State<MyBankApp> createState() => _MyBankAppState();
+}
+
+class _MyBankAppState extends State<MyBankApp> {
+  Widget _defaultScreen = RegistrationPage();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkRegistration();
+  }
+
+  Future<void> _checkRegistration() async {
+    final prefs = await SharedPreferences.getInstance();
+    final registered = prefs.getBool("registered") ?? false;
+    setState(() {
+      _defaultScreen = registered ? LoginPage() : RegistrationPage();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic App',
+      title: 'Dummy Bank App',
+      navigatorObservers: [routeObserver],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF3B5EDF),
         scaffoldBackgroundColor: const Color(0xFFF5F7FA),
         fontFamily: 'Roboto',
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        body: Center(
-          child: Text(
-            "Welcome to the Basic App!",
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
+      home: _defaultScreen,
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegistrationPage(),
+      },
     );
   }
 }
